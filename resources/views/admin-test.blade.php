76095613acccacfef6d37d980196bdbcf888475f
REKHA-SANYAL-Git <sanyalrekha0101@gmail.com>
2026-05-29 23:15:17 +0530

    Integrated AdminLTE template

---
+++ resources/views/admin-test.blade.php
@@ -0,0 +1,11 @@
+@extends('adminlte::page')
+
+@section('title', 'Admin Test')
+
+@section('content_header')
+<h1>Inventory Management System</h1>
+@stop
+
+@section('content')
+<p>AdminLTE Working Successfully.</p>
+@stop
