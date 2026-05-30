0d43898c818a390bc9e1c16061aa26afc7c10f3d
REKHA-SANYAL-Git <sanyalrekha0101@gmail.com>
2026-05-30 12:28:04 +0530

    Created warehouses migration

---
+++ database/migrations/2026_05_30_065704_create_warehouses_table.php
@@ -0,0 +1,37 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('warehouses', function (Blueprint $table) {
+
+            $table->id();
+
+            $table->string('warehouse_name');
+            $table->string('warehouse_code')->unique();
+
+            $table->text('location')->nullable();
+
+            $table->boolean('status')->default(1);
+
+            $table->timestamps();
+            $table->softDeletes();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('warehouses');
+    }
+};
