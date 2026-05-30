6637eb6d46c003d461f297c9989228552da292f1
REKHA-SANYAL-Git <sanyalrekha0101@gmail.com>
2026-05-30 12:05:21 +0530

    Created hsn master migration

---
+++ database/migrations/2026_05_30_062938_create_hsn_master_table.php
@@ -0,0 +1,36 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('hsn_master', function (Blueprint $table) {
+            $table->id();
+
+            $table->string('hsn_code', 20);
+            $table->text('hsn_description');
+
+            $table->date('effective_date')->nullable();
+
+            $table->boolean('status')->default(1);
+
+            $table->timestamps();
+            $table->softDeletes();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('hsn_master');
+    }
+};
