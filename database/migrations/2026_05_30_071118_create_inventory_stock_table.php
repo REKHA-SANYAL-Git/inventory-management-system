47ffb0464ec583c2271ccb676048bef56344a49e
REKHA-SANYAL-Git <sanyalrekha0101@gmail.com>
2026-05-30 12:44:34 +0530

    Created inventory_stock migration

---
+++ database/migrations/2026_05_30_071118_create_inventory_stock_table.php
@@ -0,0 +1,43 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('inventory_stock', function (Blueprint $table) {
+
+            $table->id();
+
+            $table->foreignId('warehouse_id')
+                ->constrained('warehouses');
+
+            $table->foreignId('sku_id')
+                ->constrained('skus');
+
+            $table->decimal('available_qty', 12, 2)
+                ->default(0);
+
+            $table->decimal('reserved_qty', 12, 2)
+                ->default(0);
+
+            $table->timestamps();
+
+            $table->unique(['warehouse_id', 'sku_id']);
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('inventory_stock');
+    }
+};
