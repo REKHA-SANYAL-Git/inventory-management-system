f5d42b599ba6217d878cac71a1d527bdc741baa6
REKHA-SANYAL-Git <sanyalrekha0101@gmail.com>
2026-06-01 08:49:36 +0530

    Definition Model Hsn Created

---
+++ app/Models/Hsn.php
@@ -0,0 +1,27 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\SoftDeletes;
+
+class Hsn extends Model
+{
+    use HasFactory;
+    use SoftDeletes;
+
+    protected $table = 'hsn_master';
+
+    protected $fillable = [
+        'hsn_code',
+        'hsn_description',
+        'effective_date',
+        'status'
+    ];
+
+    public function skus()
+    {
+        return $this->hasMany(Sku::class, 'hsn_id');
+    }
+}
